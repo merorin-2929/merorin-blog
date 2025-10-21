517e859c2bde965ec8bf3c27732e4a29202fc3c5
merorin-2929 <merorin.2929@gmail.com>
2025-10-21 16:25:21 +0900

    SEO系

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  site: 'https://blog.merorin.fun',
 
-  integrations: [mdx()]
+  integrations: [sitemap(), expressiveCode(), mdx()]
 });
